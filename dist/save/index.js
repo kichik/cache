87c8d2f3abce51a09b9032532fa3304d8cf74074
Amir Szekely <kichik@gmail.com>
2021-09-03 15:26:04 -0700

    Build again

--- dist/save/index.js
+++ dist/save/index.js
@@ -47176,10 +47176,14 @@ function run() {
                 utils.logWarning(`Error retrieving key from state.`);
                 return;
             }
-            if (utils.isExactKeyMatch(primaryKey, state)) {
-                core.info(`Cache hit occurred on the primary key ${primaryKey}, not saving cache.`);
-                return;
-            }
+            /* kichik mod
+                    if (utils.isExactKeyMatch(primaryKey, state)) {
+                        core.info(
+                            `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
+                        );
+                        return;
+                    }
+            */
             const cachePaths = utils.getInputAsArray(constants_1.Inputs.Path, {
                 required: true
             });
